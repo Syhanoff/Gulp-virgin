9d476659e4197fab5a68fd4d7876ba35fbfff7c3
Stanislav <s.syhanof@gmail.com>
2021-12-19 23:10:01 +0300

    2 папки: test и base + gitignor всему репозиторию

---
+++ Test/src/js/main.js
@@ -0,0 +1,17 @@
+// import {asd} from './func';
+
+// asd();
+
+const color = 'red'
+
+if (color == 'green') {
+
+console.log('этот цвет зеленый');
+
+} else if(color == 'red') {
+
+console.log('этот цвет красный');
+
+}
+
+1234567890
